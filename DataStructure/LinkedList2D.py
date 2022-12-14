f08f68fb4c1993f7bf94cd2c3fc5e4c93f2cef19
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-14 20:10:42 +0200

    working on 2d node

--- DataStructure/LinkedList2D.py
+++ DataStructure/LinkedList2D.py
@@ -1,6 +1,21 @@
 class LinkedList2D:
     class Node2D:
-        def __init__(self, value, nextStudent, nextCourse):
+        def __init__(self, value, student, course, nextStudent=None, nextCourse=None):
             self.value = value
+            self.student = student
+            self.course = course
             self.NextStudent = nextStudent
             self.NextCourse = nextCourse
+
+    def __init__(self):
+        self.studentSize = 0
+        self.courseSzie = 0
+        self.studentHead = None
+        self.courseHead = None
+
+    def AddStudentToCourse(self, grade, course, student):
+        newNode = self.Node2D(grade, student, course,
+                              self.studentHead, self.courseHead)
+
+        currentCourse = self.courseHead
+        while currentCourse
