f402da417876f06cac2f52395de2db9b96efb5b4
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-14 19:44:55 +0200

    init 2d linked list

---
+++ DataStructure/LinkedList2D.py
@@ -0,0 +1,6 @@
+class LinkedList2D:
+    class Node2D:
+        def __init__(self, value, nextStudent, nextCourse):
+            self.value = value
+            self.NextStudent = nextStudent
+            self.NextCourse = nextCourse
