7d71e691d71e3e7b69af1fa0f3736986087060b1
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-17 17:32:48 +0200

    CRUD for LinkedList

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -8,38 +8,67 @@ class LinkedList:
         self.head = None
         self.size = 0
 
+    def __keyExists(self, key) -> bool:
+        current = self.head
+        if current == None:
+            return False
+        while current.next != None:
+            if current.value.Id != key:
+                current = current.next
+            else:
+                return True
+        return False
+
     def lenth(self):
         return self.size
 
     def isEmpty(self):
         return self.size == 0
 
-    def AddHead(self, value):
+    def Add(self, value):
+        if self.__keyExists(value.Id):
+            raise RuntimeError('this key already exists, key:', value.Id)
         newNode = self.Node(value, self.head)
         self.head = newNode
         self.size += 1
 
-    def RemoveValue(self, value):
+    def Remove(self, key):
         if self.isEmpty():
             raise RuntimeError("list is empty")
-        if self.head.value == value:
+        if self.__keyExists(key) == False:
+            raise RuntimeError("item with key:", key, "was not found")
+        if self.head.value.Id == key:
             self.head = self.head.next
             self.size -= 1
             return True
         current = self.head
         while current.next != None:
-            if current.next.value == value:
+            if current.next.value.Id == key:
                 current.next = current.next.next
                 self.size -= 1
                 return True
             else:
                 current = current.next
-        raise RuntimeError("item with value:", value, "was not found")
 
-    def print(self):
+    def Select(self, key):
         current = self.head
-        print('---- list size :', self.size, ' ----')
-        while current != None:
-            print(current.value.Name)
-            current = current.next
-        print('---- end of list ----')
+        if current == None:
+            return None
+        while current.next != None:
+            if current.value.Id != key:
+                current = current.next
+            else:
+                return current.value
+        return None
+
+    def Update(self, key, value):
+        current = self.head
+        if current == None:
+            raise RuntimeError('data list is empty, nothing to update')
+        if self.__keyExists(key) == False:
+            raise RuntimeError("item with key:", key, "was not found")
+        while current.next != None:
+            if current.value.Id != key:
+                current = current.next
+            else:
+                current.value = value
