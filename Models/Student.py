169636ab93b2b55b67cd473a81fac40140b3f09e
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-17 17:38:51 +0200

    printable models

--- Models/Student.py
+++ Models/Student.py
@@ -2,3 +2,6 @@ class Student:
     def __init__(self, id, name):
         self.Id = id
         self.Name = name
+
+    def __str__(self) -> str:
+        return f'student with id:{self.Id}, name:{self.Name}'
