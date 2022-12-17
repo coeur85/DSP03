9d9988495c441382948363921bcc558bc8fc1ef4
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-17 22:59:22 +0200

    updated course model

--- Models/Course.py
+++ Models/Course.py
@@ -1,6 +1,13 @@
 class Course:
-    def __init__(self,id, name):
+    def __init__(self, id, name):
         self.Id = id
         self.Name = name
+
     def __str__(self) -> str:
         return f'course with id:{self.Id}, name:{self.Name}'
+
+    def __eq__(self, other) -> bool:
+        return self.Id == other.Id and self.Name == other.Name
+
+    def __repr__(self):
+        return f"course (id={repr(self.Id)}, name={repr(self.Name)})"
