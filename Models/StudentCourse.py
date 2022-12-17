bf1787d1a8a9d4b7527e66b42ccd4b42fcdca09e
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-17 23:04:08 +0200

    model for student Course

---
+++ Models/StudentCourse.py
@@ -0,0 +1,11 @@
+class StudentCourse:
+      StudentId : int
+      CourseId : int
+      Grade : int
+
+      def __init__(self,studentId,courseId,grade):
+            self.StudentId = studentId
+            self.CourseId = courseId
+            self.Grade = grade
+      def __str__(self) -> str:
+            return f'Greade :{self.Grade}'
