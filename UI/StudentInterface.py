14a9c2c496a4a6d9d5ef39f2286e5d0e06c10fee
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-18 19:03:32 +0200

    DeleteStudent interface

--- UI/StudentInterface.py
+++ UI/StudentInterface.py
@@ -1,11 +1,16 @@
-from rich.prompt import Prompt
+from rich.prompt import *
 from Context.Context import Context
+
+
 class StudentInterface():
-    def __init__(self,promot : Prompt,context : Context):
+    def __init__(self, promot: Prompt, context: Context):
         self.promote = promot
         self.context = context
-
 
     def AddNewStudent(self):
         studentName = self.promote.ask('enter student name')
         self.context.StudentsAddNew(studentName)
+
+    def DeleteStudent(self):
+        studentId = IntPrompt.ask('enter student Id')
+        self.context.StudentsDelete(studentId)
