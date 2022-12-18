5e4deda81beb618b9a63488fa9e8464673c61d4c
ahmed magdi <ahmed.magdi@bgomla.com>
2022-12-18 22:03:12 +0200

    app info

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from UI.AppUserInterface import AppUserInterface
 
 
+print('--------------------------------------------------------------------')
+print('---                                                              ---')
+print('---          name :            Ahmed Magdi                       ---')
+print('---          code :            1404-3-063                        ---')
+print('---        gitHub :            https://github.com/coeur85/DSP03  ---')
+print('---           App :            Data Structure                    ---')
+print('---                                                              ---')
+print('--------------------------------------------------------------------')
+
 ui = AppUserInterface()
 ui.HomePage()
